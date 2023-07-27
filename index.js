8c0ba6e7e49b9f989c38688282cd6ab7073c0ada
j-grosse <pixelpause@gmx.de>
2023-07-27 10:48:28 +0200

    CRUD

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ const pool = new Pool({
 });
 app.use(cors());
 app.use(express.json());
+
+///// RESTful API /////
+
+// READ
 app.get('/api/movies', (req, res) => {
   pool
     .query('SELECT * FROM movies;')
@@ -18,9 +22,11 @@ app.get('/api/movies', (req, res) => {
     })
     .catch((e) => res.status(500).json({ message: e.message }));
 });
+
+// READ by ID
 app.get('/api/movies/:id', (req, res) => {
   const { id } = req.params;
-  const safeValues = [id]; //this need to be array!!
+  const safeValues = [id]; // safe against SQL injection. This need to be array
   pool
     .query('SELECT * FROM movies WHERE id=$1;', safeValues)
     .then(({ rowCount, rows }) => {
@@ -34,28 +40,14 @@ app.get('/api/movies/:id', (req, res) => {
     })
     .catch((e) => res.status(500).json({ message: e.message }));
 });
-app.get('/api/movies/:id', (req, res) => {
-  const { id } = req.params;
-  const safeValues = [id]; //this need to be array!!
-  pool
-    .query('SELECT * FROM movies WHERE id=$1;', safeValues)
-    .then(({ rowCount, rows }) => {
-      // rowCount from the database response
-      if (rowCount === 0) {
-        res.status(404).json({ message: `Movie with id ${id} Not Found` });
-      } else {
-        console.log(rows);
-        res.json(rows[0]); // rows is array of 1 item
-      }
-    })
-    .catch((e) => res.status(500).json({ message: e.message }));
-});
+
+// CREATE
 app.post('/api/movies', (req, res) => {
-  const { title, director, year, genre } = req.body;
-  const safeValues = [title, director, year, genre]; //this needs to be array!!
+  const { title, director, year, rating, poster, genre } = req.body;
+  const safeValues = [title, director, year, rating, poster, genre];
   pool
     .query(
-      'INSERT INTO movies (title,director,year,genre) VALUES ($1,$2,$3,$4) RETURNING *;',
+      'INSERT INTO movies (title, director, year, rating, poster, genre) VALUES ($1,$2,$3,$4,$5,$6) RETURNING *;',
       safeValues
     )
     .then(({ rows }) => {
@@ -64,13 +56,15 @@ app.post('/api/movies', (req, res) => {
     })
     .catch((e) => res.status(500).json({ message: e.message }));
 });
+
+// UPDATE
 app.put('/api/movies/:id', (req, res) => {
   const { id } = req.params;
-  const { title, author, year } = req.body;
-  const safeValues = [title, author, year, id]; //this need to be array!!
+  const { title, director, year, rating, poster, genre } = req.body;
+  const safeValues = [title, director, year, rating, poster, genre, id];
   pool
     .query(
-      'UPDATE movies SET title = $1,author=$2,year=$3 WHERE id=$4 RETURNING *;',
+      'UPDATE movies SET title = $1, director=$2, year=$3, rating=$4, poster=$5, genre=$6 WHERE id=$7 RETURNING *;',
       safeValues
     )
     .then(({ rows }) => {
@@ -79,6 +73,8 @@ app.put('/api/movies/:id', (req, res) => {
     })
     .catch((e) => res.status(500).json({ message: e.message }));
 });
+
+// DELETE
 app.delete('/api/movies/:id', (req, res) => {
   const { id } = req.params;
   const safeValues = [id]; //this need to be array!!
@@ -90,4 +86,6 @@ app.delete('/api/movies/:id', (req, res) => {
     })
     .catch((e) => res.status(500).json({ message: e.message }));
 });
+
+// APP.LISTEN
 app.listen(PORT, () => console.log(`SERVER IS UP ON ${PORT}`));
